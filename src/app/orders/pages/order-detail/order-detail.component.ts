e14e35edbb23bab6d3f631996a23cd9abfaec0af
achille_comalli <achille@menatcode.it>
2024-11-05 23:15:36 +0100

    Create new orders detail page

---
+++ src/app/orders/pages/order-detail/order-detail.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {OrderService} from "../../../core/services/order.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-order-detail',
+  templateUrl: './order-detail.component.html',
+  styleUrls: ['./order-detail.component.scss']
+})
+export class OrderDetailComponent implements OnInit {
+  order: any;
+  params: any;
+
+  constructor(
+    private orderService: OrderService,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.activatedRoute.params.subscribe((params: any) => {
+      this.params = params;
+    });
+  }
+
+  ngOnInit(): void {
+    this.orderService.getOrderById( this.params.id).subscribe(
+      (result) => {
+        this.order = result.data;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+}
